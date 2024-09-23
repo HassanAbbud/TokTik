3d078d6614bd48722ab4120d9c4fb7b8c2639827
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-23 17:10:19 -0600

    Animate icon with dependency "animate_do"

--- lib/presentation/widgets/video_buttons.dart
+++ lib/presentation/widgets/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/videos_post.dart';
@@ -13,7 +14,13 @@ class VideoButtons extends StatelessWidget {
     return Column(
       children: [
         _CustomIconButton(value: video.likes, iconData: Icons.favorite, color: Colors.red),
+        const SizedBox(height: 10,),
         _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye),
+        const SizedBox(height: 10,),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(iconData: Icons.filter_tilt_shift)),
       ],
     );
   }
@@ -27,7 +34,7 @@ class _CustomIconButton extends StatelessWidget {
 
   const _CustomIconButton({
     super.key,
-    required this.value,
+    this.value = 0,
     required this.iconData,
     this.color = Colors.white,
   });
@@ -36,7 +43,9 @@ class _CustomIconButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        IconButton(onPressed: () {}, icon: Icon(iconData, color: color, size: 40,)),
+        IconButton(onPressed: () {}, icon: Icon(iconData, color: color, size: 35,)),
+
+        if(value > 0)
         Text(HumanFormats.humanReadableNumber(value.toDouble()))
       ],
     );
