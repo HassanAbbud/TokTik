de92539abd2660e4a774f43a75f2e5f2225ff653
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-24 10:34:39 -0600

    Datasource and repositories

---
+++ lib/domain/datasource/video_posts_datasource.dart
@@ -0,0 +1,10 @@
+import 'package:toktik/domain/entities/videos_post.dart';
+
+abstract class VideoPostDatasource {
+
+  Future<List<VideoPost>> getFavoriteVideosByUser( String userID );
+
+  Future<List<VideoPost>> getTrendingVideosByPage( int page );
+
+
+}
